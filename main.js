0b7ab4d51a2afdcf512b4333e94bb7e5d719caa8
Solange <sbrionesg@unemi.edu.ec>
2021-01-06 14:30:57 -0500

    Se añadió instrucciones a main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+/* Sintaxis del lenguaje variables y tipos de datos */
+console.log("Funcionando...")
+var numero=10
+let num1=null //undefined
+let num2=null //nulo
+let entero = 10
+let decimal = 1.2
+let string = "Hello world"
+let booleano = false
+let fecha = new Date()
+let dd = fecha. getDate(), mm = fecha.getMonth() +1, aa = fecha.getFullYear(), hoy = `${aa}-${mm}-${dd}`
+const pi =3.1416 //constante
+console.log("Numero1 =", num1)
+console.log(`Número2 = ${num2}`)
+console.log(`fecha hoy= ${fecha}`)
+console.log(hoy)
